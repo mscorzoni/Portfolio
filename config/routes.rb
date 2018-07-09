64af5577c0d8f28306a0f0b4ac28a5abc4da2b75
Marcos Scorzoni <mlessa@gmail.com>
2018-07-09 19:41:30 +0100

    customize portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolio1s
+  resources :portfolio1s, except: [:show]
+  get 'portfolio/:id', to: 'portfolio1s#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
 
