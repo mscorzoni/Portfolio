f947c6777bb61ea5045539ec132ecabb03850bb8
Marcos Scorzoni <mlessa@gmail.com>
2018-07-10 16:59:23 +0100

    nested attributes for portfolios

--- app/models/portfolio1.rb
+++ app/models/portfolio1.rb
@@ -1,6 +1,7 @@
 class Portfolio1 < ApplicationRecord
   include Placeholder
   has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :ruby_on_rails_portfolio_items,  -> { where(subtitle: 'Ruby on Rails') }
