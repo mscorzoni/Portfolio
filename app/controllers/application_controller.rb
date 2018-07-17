db4211800ee1da23d150db24d4b2a408d13228d0
Marcos Scorzoni <mlessa@gmail.com>
2018-07-17 13:43:56 +0100

    add copyright at homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = MslViewTool::Renderer.copyright 'Marcos Scorzoni', 'All rights reserved'
+  end
+end
+
+module MslViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
