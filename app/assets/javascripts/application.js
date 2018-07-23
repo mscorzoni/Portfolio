c373ccc6135d0a1f7391f7332439af3c0d824136
Marcos Scorzoni <mlessa@gmail.com>
2018-07-23 13:36:50 +0100

    install jquery ui, sortable html5 library, and css call

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require bootstrap-sprockets
 //= require rails-ujs
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
