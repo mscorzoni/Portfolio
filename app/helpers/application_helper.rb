384f4b852dcab3a0a94922ba55ec1e0325b2a578
Marcos Scorzoni <mlessa@gmail.com>
2018-07-31 20:53:14 +0100

    fix bug style in home navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to 'Register', new_user_registration_path, class: style) +
-      "<br>".html_safe +
+      " ".html_safe +
       (link_to 'Login', new_user_session_path, class: style)
     else
       link_to 'Logout', destroy_user_session_path, method: :delete, class: style
@@ -55,11 +55,11 @@ module ApplicationHelper
     ]
   end
 
-  def nav_helper style, tag_type
+ def nav_helper style, tag_type
     nav_links = ''
 
     nav_items.each do |item|
-     nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a><#{tag_type}>" 
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
     nav_links.html_safe
@@ -81,4 +81,6 @@ module ApplicationHelper
     js add_gritter(msg, title: "Marcos Scorzoni Portfolio", sticky: false)
   end  
 end
+
+
 
